c2da27dafe138dd9fe0a1163525b6951c89872af
kaiyork neo <kaiyork@live.com.sg>
2024-02-18 21:53:25 +0800

    created security group,created template to draw ami

--- main.tf
+++ main.tf
@@ -39,14 +39,41 @@ resource "aws_internet_gateway" "internet_gw" {
   }
 }
 resource "aws_route_table" "dev_public_rt" {
-    vpc_id = aws_vpc.main.id
-    tags = {
+  vpc_id = aws_vpc.main.id
+  tags = {
     Name = "dev_public_rt"
-    } 
+  }
 }
 
 resource "aws_route" "default_route" {
-  route_table_id            = aws_route_table.dev_public_rt.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.internet_gw.id
+  route_table_id         = aws_route_table.dev_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gw.id
+}
+
+resource "aws_route_table_association" "dev_public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.dev_public_rt.id
+
+}
+
+resource "aws_security_group" "dev_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
 }
