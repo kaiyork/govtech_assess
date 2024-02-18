bd0091a5219e700c358ac64e784701d7053097ce
kaiyork neo <kaiyork@live.com.sg>
2024-02-18 23:22:37 +0800

    recreated vpc with 2 private and 2 public subnets for availibility

--- main.tf
+++ main.tf
@@ -9,20 +9,31 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-southeast-1a"
   tags = {
-    Name = "dev-public"
+    Name = "dev-public-1"
   }
 
 }
 
-resource "aws_subnet" "private_subnet" {
+resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-southeast-1b"
+  tags = {
+    Name = "dev-public-2"
+  }
+
+}
+
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-southeast-1a"
   tags = {
@@ -31,69 +42,99 @@ resource "aws_subnet" "private_subnet" {
 
 }
 
+resource "aws_subnet" "private-subnet-2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-southeast-1b"
+  tags = {
+    Name = "dev-private-2"
+  }
+
+}
+
 resource "aws_internet_gateway" "internet_gw" {
   vpc_id = aws_vpc.main.id
-
   tags = {
     Name = "dev_internet_gateway"
-  }
-}
-resource "aws_route_table" "dev_public_rt" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "dev_public_rt"
-  }
+    }
 }
 
-resource "aws_route" "default_route" {
-  route_table_id         = aws_route_table.dev_public_rt.id
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "public-route-table"
+  }
+}
+resource "aws_route" "public-route" {
+  route_table_id         = aws_route_table.public-route-table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.internet_gw.id
 }
 
-resource "aws_route_table_association" "dev_public_association" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.dev_public_rt.id
-
+resource "aws_route_table_association" "public-subnet-1-association" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public-route-table.id
 }
 
-resource "aws_security_group" "dev_sg" {
-  name        = "dev_sg"
-  description = "dev security group"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
+resource "aws_route_table_association" "public-subnet-2-association" {
+  subnet_id      = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.public-route-table.id
 }
 
-resource "aws_key_pair" "terra_auth" {
-    key_name = "authkey"
-    public_key = file("~/.ssh/authkey.pub")  
-}
 
-resource "aws_instance" "dev_node" {
-    instance_type = "t2.micro"
-    ami = data.aws_ami.ubuntu.id
-    tags = {
-      name ="dev-node"
-    }
-    key_name = aws_key_pair.terra_auth.id
-    vpc_security_group_ids = [aws_security_group.dev_sg.id]
-    subnet_id = aws_subnet.public_subnet.id
-    root_block_device {
-      volume_size = 10
-    }
-  
-}
+
+# resource "aws_route" "default_route" {
+#   route_table_id         = aws_route_table.dev_public_rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.internet_gw.id
+# }
+
+# resource "aws_route_table_association" "dev_public_association" {
+#   subnet_id      = aws_subnet.public_subnet.id
+#   route_table_id = aws_route_table.dev_public_rt.id
+
+# }
+
+# resource "aws_security_group" "dev_sg" {
+#   name        = "dev_sg"
+#   description = "dev security group"
+#   vpc_id      = aws_vpc.main.id
+
+#   ingress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+
+# }
+
+# resource "aws_key_pair" "terra_auth" {
+#   key_name   = "authkey"
+#   public_key = file("~/.ssh/authkey.pub")
+# }
+
+# resource "aws_instance" "dev_node" {
+#   instance_type = "t2.micro"
+#   ami           = data.aws_ami.ubuntu.id
+#   key_name               = aws_key_pair.terra_auth.id
+#   vpc_security_group_ids = [aws_security_group.dev_sg.id]
+#   subnet_id              = aws_subnet.public_subnet.id
+#   user_data = file("userdata.tpl")
+#   root_block_device {
+#     volume_size = 10
+#   }
+#   tags = {
+#     name = "dev-node"
+#   }
+
+# }
+
